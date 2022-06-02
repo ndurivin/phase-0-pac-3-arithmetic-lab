827a18e204293ce55683f6c7f8112444aa77e469
Vinn Nduri <ndurivo@gmail.com>
2022-06-02 23:13:41 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,48 @@
+
+function add(){
+    return (a + b);
+}
+
+function subtract(){
+    return (x - y);
+}
+
+function multiply(){
+    return (a * b);
+}
+
+function divide(){
+    return (x / y);
+}
+
+function add(a,b){
+    return (a + b);
+}
+
+function subtract(a, b){
+    return (a - b);
+}
+
+function multiply(a, b){
+    return (a * b);
+}
+
+function divide(a, b){
+    return (a / b);
+}
+
+function increment(n){
+    return (n + 1);
+}
+
+function decrement(n){
+    return (n - 1);
+}
+
+function makeInt(n){
+    return (parseInt(n, 10));
+}
+
+function preserveDecimal(n){
+    return (parseFloat(n));
+}
